90cb08fe7d16de9f5e35113d3945263544f03540
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2026-07-12 09:13:49 -0300

    refactor: move database and telemetry to src/infrastructure/
    - Relocate src/database/ to src/infrastructure/database/
    - Relocate src/telemetry/ to src/infrastructure/telemetry/
    - Add query-executor.ts interface
    - Add unit-of-work.postgres.ts implementation
    - Update eslint config for new structure
    - Remove stale startTransaction tests (replaced by runInTransaction)

--- src/infrastructure/telemetry/opentelemetry.config.ts
+++ src/infrastructure/telemetry/opentelemetry.config.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import * as dotenv from 'dotenv';
 import * as os from 'node:os';
