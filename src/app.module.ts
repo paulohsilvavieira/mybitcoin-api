5e93eb95948283f599e3103bec0157f51d2e4203
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2026-07-12 09:14:19 -0300

    chore: update app module and main.ts for new structure
    - Import DatabaseModule from src/infrastructure/database/
    - Import FinancialModule from src/modules/financial/
    - Update telemetry imports to src/infrastructure/telemetry/

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './infrastructure/database/database.module';
 import { HealthController } from './health.controller';
+import { FinancialModule } from './modules/financial/financial.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    FinancialModule,
+  ],
   controllers: [HealthController],
   providers: [],
 })
