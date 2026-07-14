dbab1b9d75a78b55bd38197e5a1e90cb28441d2c
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2026-07-14 18:53:50 -0300

    fix: remove wrong tests

--- src/modules/identity/identity.module.ts
+++ src/modules/identity/identity.module.ts
@@ -5,8 +5,6 @@ import { PgUserRepository } from '@/modules/identity/infrastructure/persistence/
 import { UserRepository } from '@/modules/identity/domain/repositories/user.repository';
 import { EmailService } from '@/modules/identity/domain/services/email.service';
 import { QueryExecutor } from '@/infrastructure/database/query-executor';
-import { LoggerPort, LOGGER_PORT } from '@/shared/logger.port';
-import { OtelLoggerAdapter } from '@/infrastructure/telemetry/otel-logger.adapter';
 import * as bcrypt from 'bcrypt';
 
 @Module({
@@ -23,25 +21,17 @@ import * as bcrypt from 'bcrypt';
         sendVerification: () => Promise.resolve(),
       }),
     },
-    {
-      provide: LoggerPort,
-      useClass: OtelLoggerAdapter,
-    },
+
     {
       provide: RegisterUser,
-      useFactory: (
-        userRepo: UserRepository,
-        emailService: EmailService,
-        logger: LoggerPort,
-      ) => {
+      useFactory: (userRepo: UserRepository, emailService: EmailService) => {
         return new RegisterUser(
           userRepo,
           emailService,
           (plain: string) => bcrypt.hash(plain, 12) as Promise<string>,
-          logger,
         );
       },
-      inject: [UserRepository, EmailService, LOGGER_PORT],
+      inject: [UserRepository, EmailService],
     },
   ],
 })
