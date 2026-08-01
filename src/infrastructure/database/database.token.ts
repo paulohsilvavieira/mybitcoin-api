78eff66a11e6a18285bbae779be01646c08985e7
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2026-08-01 16:07:05 -0300

    feat: implements read  executor and query replica

--- src/infrastructure/database/database.token.ts
+++ src/infrastructure/database/database.token.ts
@@ -1,1 +1,2 @@
-export const POOL_TOKEN = Symbol('POOL_TOKEN');
+export const WRITE_POOL_TOKEN = Symbol('WRITE_POOL_TOKEN');
+export const READ_POOL_TOKEN = Symbol('READ_POOL_TOKEN');
---
+++ src/infrastructure/database/read-query-executor.ts
@@ -0,0 +1,3 @@
+import { QueryExecutor } from '@/infrastructure/database/query-executor';
+
+export abstract class ReadQueryExecutor extends QueryExecutor {}
